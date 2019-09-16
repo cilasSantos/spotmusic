167b54d53f0d3b9d68efe1c991af7c1c29f6e8b9
cilasSantos <cilasSantos@users.noreply.github.com>
2019-09-16 20:24:37 -0300

    header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
+import Header from './components/Header'
 
-import { Wapper, Container } from './styles/components'
+import { Wapper, Container, Content } from './styles/components'
 const App = () => (
   <Wapper>
     <Container>
       <Sidebar />
+      <Content>
+        <Header />
+      </Content>
     </Container>
     <Player />
   </Wapper>
